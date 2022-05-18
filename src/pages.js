5f57cad444fa0627d45111c89eef4e211469832a
dcr041 <95144365+dcr041@users.noreply.github.com>
2022-05-17 19:01:23 -0700

    Update pages.js and add movies.js and addReviewForm.js

--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { MovieList } from "./movies";
+import { AddReviewForm } from "./addReviewForm";
 
 export function Home({movies, setMovies}) {
-
+    return (
+        <>
+            <h1>Movie Reviews</h1>
+            <nav>
+                <Link to="/addReview">Add Review</Link>
+            </nav>
+            <br></br>
+            <MovieList
+                movies={movies}
+                onRemoveMovie = {
+                    movieName => {
+                        const newMovies = movies.filter(movie => movie.name !== movieName);
+                        setMovies(newMovies);
+                    }
+                }
+            />
+        </>
+    );
 }
 
 export function AddReview({movies, setMovies}) {
-    
+    return (
+        <>
+            <h1>Add Review</h1>
+            <nav>
+                <Link to="/">Home</Link>
+            </nav>
+            <br></br>
+            <AddReviewForm 
+                onNewMovie={(name, date, stars, poster, rating) => {
+                    const newReviews = [...movies, {name, date, stars, poster, rating}];
+                    setMovies(newReviews);
+                }}
+            />
+        </>
+    );
 }
